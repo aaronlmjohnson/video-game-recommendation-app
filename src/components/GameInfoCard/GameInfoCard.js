72e142160223f6e2c01685cce141fe5c0f6a7dd2
Aaron <ajonaaronajon@yahoo.com>
2022-09-24 17:35:09 -0400

    installed axios

--- src/components/GameInfoCard/GameInfoCard.js
+++ src/components/GameInfoCard/GameInfoCard.js
@@ -1,6 +1,16 @@
-const GameInfoCard = ()=>{
+const GameInfoCard = (props)=>{
     return(
-        <></>
+        <div className="game-info-card">
+            <h1 className="game-name">
+                Grand Theft Auto 5
+            </h1>
+            <p className="game-release-date">
+                9/17/2013
+            </p>
+            <p className="game-developer">
+                Rockstar
+            </p>
+        </div>
     );
 }
 
--- src/components/GameInfoCard/GameInfoCard.test.js
+++ src/components/GameInfoCard/GameInfoCard.test.js
@@ -3,20 +3,26 @@ import GameInfoCard from "./GameInfoCard";
 
 describe('Game Info Card', ()=>{
     it('renders an image of the game"', ()=>{
-
+        render(<GameInfoCard />)
     });
 
-    describe('Game Info', ()=>{
-        it("displays name of game", ()=>{
-            
+    describe('Game Info for GTA5', ()=>{
+        it("displays name of game titled 'Grand Theft Auto 5'", ()=>{
+            render(<GameInfoCard />);
+            const name = screen.getByRole('heading', {name: "Grand Theft Auto 5"});
+            expect(name.textContent).toMatch(/Grand Theft Auto 5/i);
         });
 
-        it("displays the release date", ()=>{
-
+        it("displays the release date '9/17/2013'", ()=>{
+            render(<GameInfoCard />);
+            const releaseDate = screen.getByText("9/17/2013");
+            expect(releaseDate.textContent).toMatch(/9\/17\/2013/i);
         });
 
         it("displays developer name", ()=>{
-
+            render(<GameInfoCard />);
+            const developer = screen.getByText("Rockstar");
+            expect(developer.textContent).toMatch(/Rockstar/i);
         });
     });
     
