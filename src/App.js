94cd77d8ed1e915a3b0e337294892294ad52469b
Aaron <ajonaaronajon@yahoo.com>
2022-10-25 20:14:14 -0400

    added year state and input field

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import ModeButtons from './components/ModeButtons/ModeButtons';
 import PopularGames from './components/PopularGames/PopularGames';
 import PopularGameData from './data.json';
+import { useState } from 'react';
 
 function App() {
-  console.log(PopularGameData.results);
+  const [year, setYear] = useState(new Date().getFullYear());
+
+  const updateYear = (value)=>{
+    setYear(value);
+  }
   return (
     <div id="app-container">
       <h1>Video Game Recommender</h1>
       <ModeButtons />
-      <PopularGames data={PopularGameData.results}/>
+      <PopularGames 
+        data={PopularGameData.results}
+        year = {year}
+        updateYear = {updateYear}
+      />
     </div>
   );
 }
--- src/components/PopularGames/PopularGames.js
+++ src/components/PopularGames/PopularGames.js
@@ -1,17 +1,25 @@
 import GameInfoCard from '../GameInfoCard/GameInfoCard';
 
-const PopularGames = ({data})=>{
+const PopularGames = ({data, year, updateYear})=>{
     return (
         <div id="popular-games-for-this-year">
             <h1>Popular Games of YEAR</h1>
-            {data.map((game)=>{
-            return <GameInfoCard 
-                name={game.name}
-                releaseDate = {game.released}
-                rating={game.rating} 
-                src={game.background_image} 
-                key={game.id}
+            <input 
+                type="number" 
+                min="1971" 
+                max="2022"
+                value={year}
+                onChange={(e)=>updateYear(e.target.value)}
             />
+
+            {data.map((game)=>{
+                return <GameInfoCard 
+                    name={game.name}
+                    releaseDate = {game.released}
+                    rating={game.rating} 
+                    src={game.background_image} 
+                    key={game.id}
+                />
             })}
         </div>
     );
