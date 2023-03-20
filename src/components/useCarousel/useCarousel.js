21c447dc9ef1aeb19346726b9dc0df7ed663c7e0
Aaron Johnson <aaronlmjohnson307@gmail.com>
2023-03-20 11:11:14 -0500

    fixed carousel animations for cycling left

--- src/components/useCarousel/useCarousel.js
+++ src/components/useCarousel/useCarousel.js
@@ -12,6 +12,14 @@ const useCarousel = ()=>{
 
     const SUBSET_SIZE = 5;
 
+    const animationsLeft = [
+        "left-next-to-edge",
+        "active-to-left-next",
+        "right-next-to-active",
+        "right-edge-to-next",
+        "fade-in-right-edge"
+    ]
+
     // useEffect(()=>{
     //     if(games) setSubset(games);
     // }, [games]);
@@ -60,6 +68,16 @@ const useCarousel = ()=>{
 
 
     const animationNames = (i)=>{
+        //this commented out code has an infinite loop because im incrementing start. itll never reach end condition. 
+        // let start = -1 * Math.floor(SUBSET_SIZE / 2);
+        // //loop through this start at  -Subset_size / 2 floored increment to -Subset_size / 2 floored + subset_size
+        // for(start; start < start + SUBSET_SIZE; start++){
+        //     console.log(start);
+        //     if(mod(i, games.length) === mod(index - start, games.length)) 
+        //         return `${direction === "left" ? animationsLeft[start] : "fade-in-left-edge"}`;
+        //     else
+        //         return '';
+        // }
         if(mod(i, games.length) === mod(index - 2, games.length)) 
             return `${direction === "left" ? "left-next-to-edge" : "fade-in-left-edge"}`;
         if(mod(i, games.length) === mod(index - 1, games.length)) 
@@ -71,23 +89,6 @@ const useCarousel = ()=>{
         if(mod(i, games.length) === mod(index + 2, games.length)) 
             return `${direction === "left" ? "fade-in-right-edge" : "right-next-to-edge"}`;
         return '';
-        // could make it work like onDisplay does4
-        // let animations = direction === "left" ? 
-        // [
-        //     "left-next-to-edge",
-        //     "active-to-left-next",
-        //     "right-next-to-active",
-        //     "right-edge-to-next",
-        //     "fade-in-right-edge"
-        // ] : direction === "right" ? [
-        //     "fade-in-left-edge",
-        //     "left-edge-to-next",
-        //     "left-next-to-active",
-        //     "active-to-right-next",
-        //     "right-next-to-edge"
-        // ] : [];
-
-        // return animations[i] || '';
     }
 
     const mod = (value, n)=> ((value % n) + n) % n
