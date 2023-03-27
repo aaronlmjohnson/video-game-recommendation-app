74e1cdf961bfcbfa79d7cdc0ec40aabd1dd72c0f
Aaron Johnson <aaronlmjohnson307@gmail.com>
2023-03-27 14:51:59 -0500

    added find a game form and removed comments in App.js

--- src/App.js
+++ src/App.js
@@ -1,93 +1,34 @@
 import './App.css';
-// import ModeButtons from './components/ModeButtons/ModeButtons';
-// import PopularGamesSection from './components/PopularGamesSection/PopularGamesSection';
-// import useRandomGame from './components/useRandomGame/useRandomGame';
-
-// import GameRecommendationForm from './components/GameRecommendationForm/GameRecommendationForm';
-// import platforms from './platforms.json';
-// import genres from './genres.json';
-// import developers from './developers.json';
-// import useRecommendedGames from './components/useRecommendedGames/useRecommendedGames';
-// import RecommendedGames from './components/RecommendedGames/RecommendedGames';
-// import useCarousel from './components/useCarousel/useCarousel';
-
-// function App() {
-//   const RAWG_API_KEY = process.env.REACT_APP_RAWG_API_KEY;
-
-//   // const [formData, setFormData] = useState({platforms, genres, developers}); 
-//   // const GAMES_URL = `https://api.rawg.io/api/games?key=${RAWG_API_KEY}`;
-//   // const {recommendedGames, fetchRecommendedGames, recommendedGamesLoading} = useRecommendedGames();
-//   // const [popularGameData, setPopularGameData] = useState({});
-//   // const {gameData, getRandomGame, gameDataIsEmpty, clearGameData, gameNotFound, loading} = useRandomGame();
-//   // const [renderGameForm, setRenderGameForm] = useState(false);
-//   // const {generateSubset, setGames, subset} = useCarousel([]);
-
-//   // useEffect(()=>{
-//   //   //setPopularGameData(data);
-//   //   //setRenderGameForm(false);
-//   // }, []); 
-
-
-
-//   // const handleSubmit = (e, queryData)=>{
-//   //   e.preventDefault();
-//   //   const formData = new FormData(e.target);
-//   //   fetchRecommendedGames(formData);
-//   // }
-
-//   return (
-//     <div id="app-container">
-// {/* 
-//       <ModeButtons 
-//         getRandomGame={getRandomGame}
-//         setRenderGameForm={setRenderGameForm}
-//       />
-//       <GamePage 
-//         data={gameData} 
-//         loading={loading}
-//         clearGameData={clearGameData} 
-//         gameDataIsEmpty = {gameDataIsEmpty}
-//         gameNotFound ={gameNotFound}
-//       />
-
-//        <PopularGamesSection
-//         apiKey = {RAWG_API_KEY} 
-//       /> 
- 
-//       {renderGameForm && <GameRecommendationForm 
-//           data={formData}
-//           handleSubmit={handleSubmit}
-//           setRenderGameForm={setRenderGameForm}
-//           loading={recommendedGamesLoading}
-//       />}
-
-//       {!recommendedGamesLoading()? <RecommendedGames data={recommendedGames} /> : <>Loading</> }       */}
-      
-//     </div>
-//   );
-// }
-
-import './App.css';
 import PopularGamesSection from './components/PopularGamesSection/PopularGamesSection';
 import Navbar from './components/Navbar/Navbar';
-// import useRandomGame from './components/useRandomGame/useRandomGame';
 import GamePage from './components/GamePage/GamePage';
 import { useEffect, useState } from 'react';
 import useGamePageData from './components/CustomHooks/useGamePageData';
 import GameNotFound from './components/GamePage/GameNotFound';
+import GameRecommendationForm from './components/GameRecommendationForm/GameRecommendationForm';
+import platforms from './platforms.json';
+import genres from './genres.json';
+import developers from './developers.json';
+import useRecommendedGames from './components/useRecommendedGames/useRecommendedGames';
+import RecommendedGames from './components/RecommendedGames/RecommendedGames';
 
 function App () {
 
   const RAWG_API_KEY = process.env.REACT_APP_RAWG_API_KEY;
   const GAMES_URL = `https://api.rawg.io/api/games?key=${RAWG_API_KEY}`;
-  //const {gameData, screenshots, screenshotsLoading, loading} = useGamePageData("3498");
-  //GTAV ID 3498
   const [year, setYear] = useState(new Date().getFullYear()); 
   const [renderGameForm, setRenderGameForm] = useState(false);
   const [gamePageOpen, setGamePageOpen] = useState(false);
+  const [formData, setFormData] = useState({platforms, genres, developers}); 
+  const {recommendedGames, fetchRecommendedGames, recommendedGamesLoading} = useRecommendedGames();
   const game = useGamePageData();
- //const {gameData, getRandomGame, loading} = useRandomGame();
-  // const {gameData, getRandomGame, gameDataIsEmpty, clearGameData, gameNotFound, loading} = useRandomGame();
+
+  const handleSubmit = (e, queryData)=>{
+    e.preventDefault();
+    
+    const formData = new FormData(e.target);
+    fetchRecommendedGames(formData);
+  }
   
   return (
     <div id="app-container">
@@ -119,11 +60,18 @@ function App () {
          setGamePageOpen= {setGamePageOpen}
          game={game}
       /> }
-    
 
+      {renderGameForm && <GameRecommendationForm 
+        data={formData}
+        handleSubmit={handleSubmit}
+        setRenderGameForm={setRenderGameForm}
+        loading={recommendedGamesLoading}
+      />}
+
+    {!recommendedGamesLoading()? <RecommendedGames data={recommendedGames} /> : <>Loading</> }
+    
     </div>
   );
-
 }
 
 export default App;
--- src/components/RecommendedGames/RecommendedGames.js
+++ src/components/RecommendedGames/RecommendedGames.js
@@ -1,18 +1,14 @@
-import GameInfoCard from '../GameInfoCard/GameInfoCard';
-
 const RecommendedGames = ({data, loading})=>{
     return (
         <div id="popular-games-for-this-year">
             {data && data.length > 0 ? <h1>You might like these games:</h1> : <></>}
 
             {data && data.map((game)=>{
-                return <GameInfoCard 
-                    name={game.name}
-                    releaseDate = {game.released}
-                    rating={game.rating} 
-                    src={game.background_image} 
-                    key={game.id}
-                />
+                return (<div key={game.id}>
+                        <img src={game.background_image}/>
+                        <p>{game.name}</p>
+                        
+                    </div>)
             })}
         </div>
     );
