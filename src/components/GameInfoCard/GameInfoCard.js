f9d0c9cca1328f46be7de349039c9c667e9ffa93
Aaron <aaronlmjohnson307@gmail.com>
2024-01-24 16:56:21 -0500

    replaced image in game info card with background image

--- src/components/GameInfoCard/GameInfoCard.js
+++ src/components/GameInfoCard/GameInfoCard.js
@@ -5,15 +5,11 @@ const GameInfoCard = (props)=>{
     const {src, name, carouselClass } = props;
     return(
         //get rid of img and set div's background image instead
-        <div className={`game-info-card ${carouselClass}`}>
-            <img className="game-image" src={src} alt={'#'} />
-            {
-                 
-                <div className="game-name">
-                        <p>{name}</p>
-                </div>
-            }
-            
+        <div className={`game-info-card ${carouselClass}`} style={{backgroundImage: `url(${src})`}}>
+            <div className="card-overlay"></div>
+            <div className="game-name">
+                <p>{name}</p>
+            </div>  
         </div>
     );
 }
