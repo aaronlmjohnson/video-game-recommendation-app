e1526bdbec912af1834089c85c99034e9bc2b944
Aaron <aaronlmjohnson307@gmail.com>
2024-01-24 14:41:01 -0500

    updated carousel animations

--- src/components/GameCarousel/GameCarousel.js
+++ src/components/GameCarousel/GameCarousel.js
@@ -8,11 +8,15 @@ import { useState } from 'react';
 const GamesCarousel = ({url})=>{
     const {data:games, loading:gamesLoading, error} = useApiHandler(`${url}&dates=2024-01-01,2024-12-31&page_size=16`);
     const [shift, setShift] = useState(0);
+    const [shiftClass, setShiftClass] = useState('');
     const mod = (n, d)=> ((n % d) + d) % d;
     
     return (
         <div className="game-carousel">
-            <button className="carousel-button carousel-left" onClick={()=> setShift(prev => prev -= 1)}>
+            <button className="carousel-button carousel-left" onClick={()=> {
+                setShiftClass('shift-left');
+                setShift(prev => prev -= 1)
+            }}>
                 <FontAwesomeIcon className="carousel-caret" icon={faCaretLeft} />
             </button>
             {gamesLoading ?
@@ -25,10 +29,13 @@ const GamesCarousel = ({url})=>{
                         src={game.background_image} 
                         key={game.id}
                         id={game.id}
-                        carouselClass = {shiftedI > 4 ? "hidden" : `game-info-card-${shiftedI}`}
+                        carouselClass = {shiftedI > 4 ? "hidden" : `game-info-card-${shiftedI} ${shiftClass}`}
                     />)
             })}
-            <button className="carousel-button carousel-right" onClick={()=> setShift(prev => prev -= 1)}>
+            <button className="carousel-button carousel-right" onClick={()=> {
+                setShiftClass('shift-right');
+                setShift(prev => prev += 1)
+            }}>
                 <FontAwesomeIcon className="carousel-caret" icon={faCaretRight} />
             </button>
         </div>
