6e865bb5cb55db258b2765a6ed63a96e2c00a938
Aaron <aaronlmjohnson307@gmail.com>
2024-01-24 14:22:32 -0500

    updated how carousel rotates

--- src/components/GameCarousel/GameCarousel.js
+++ src/components/GameCarousel/GameCarousel.js
@@ -3,29 +3,34 @@ import GameInfoCard from "../GameInfoCard/GameInfoCard";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons'
 import useApiHandler from '../useGameAPI/useApiHandler';
+import { useState } from 'react';
 
 const GamesCarousel = ({url})=>{
     const {data:games, loading:gamesLoading, error} = useApiHandler(`${url}&dates=2024-01-01,2024-12-31&page_size=16`);
+    const [shift, setShift] = useState(0);
+    const mod = (n, d)=> ((n % d) + d) % d;
+    
     return (
         <div className="game-carousel">
-            {/* <button className="carousel-button carousel-left" onClick={shiftLeft}>
+            <button className="carousel-button carousel-left" onClick={()=> setShift(prev => prev -= 1)}>
                 <FontAwesomeIcon className="carousel-caret" icon={faCaretLeft} />
-            </button> */}
+            </button>
             {gamesLoading ?
             <>Loading...</> :
             games?.results.map((game, i)=>{
+                let shiftedI = mod(i + shift, games.results.length);
                 return (
                     <GameInfoCard
                         name={game.name} 
                         src={game.background_image} 
                         key={game.id}
                         id={game.id}
-                        carouselClass = {i > 4 ? "hidden" : `game-info-card-${i}`}
+                        carouselClass = {shiftedI > 4 ? "hidden" : `game-info-card-${shiftedI}`}
                     />)
             })}
-            {/* <button className="carousel-button carousel-right" onClick={shiftRight}>
+            <button className="carousel-button carousel-right" onClick={()=> setShift(prev => prev -= 1)}>
                 <FontAwesomeIcon className="carousel-caret" icon={faCaretRight} />
-            </button> */}
+            </button>
         </div>
     );
 }
