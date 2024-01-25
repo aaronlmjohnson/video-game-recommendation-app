ffacef4da952b8fa3c0778d3a31f3bcb46415a14
Aaron <aaronlmjohnson307@gmail.com>
2024-01-25 12:08:16 -0500

    the front facing game card in the carousel displays text. removed from other cards

--- src/components/GameInfoCard/GameInfoCard.js
+++ src/components/GameInfoCard/GameInfoCard.js
@@ -6,8 +6,7 @@ const GameInfoCard = (props)=>{
     return(
         //get rid of img and set div's background image instead
         <div className={`game-info-card ${carouselClass}`} style={{backgroundImage: `url(${src})`}}>
-            <div className="card-overlay"></div>
-            <div className="game-name">
+            <div className={`game-name ${carouselClass.split(" ")[0] !== "game-info-card-2" ? "hidden" : ""}`}>
                 <p>{name}</p>
             </div>  
         </div>
