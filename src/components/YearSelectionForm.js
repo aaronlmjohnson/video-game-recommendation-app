44b703cb8701f81346c6f02fa2f198d386daf99e
Aaron <aaronlmjohnson307@gmail.com>
2024-02-09 17:18:59 -0500

    added styling to year selection form

--- src/components/YearSelectionForm.js
+++ src/components/YearSelectionForm.js
@@ -1,8 +1,30 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+
 const YearSelectionForm = ()=>{
+    const handleYear = (e)=>{
+        e.preventDefault();
+        console.log("year")
+    }
     return(
-        <div className="year-selection-form">
+        <div className="year-selection-form-container">
+            <form className="year-selection-form">
+                <div className="year-container">
+                    <button onClick={handleYear}>
+                    <FontAwesomeIcon icon={faAngleLeft} />
+                    </button>
+                    <input value={2024} />
+                    <button onClick={handleYear}>
+                        <FontAwesomeIcon icon={faAngleRight} />
+                    </button>
+                </div>
             
+            <button className="submit" type="submit">
+                Search
+            </button>
+        </form>
         </div>
+       
     )
 }
 
