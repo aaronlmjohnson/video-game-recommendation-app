24935041c44de4261141cb5b356ff099f25d5350
Aaron <aaronlmjohnson307@gmail.com>
2024-02-09 14:44:21 -0500

    updated bento container layout and styling

--- src/components/BentoContainer.js
+++ src/components/BentoContainer.js
@@ -5,21 +5,38 @@ import BentoNode from "./BentoNode";
 const BentoContainer = ({ data })=>{
     const handleNodes = (count)=>{
         const nodeArr = [];
-
+        //handle adding data
         for(let i = 0; i < count; i++){
-            
+            nodeArr.push(
+                <BentoNode 
+                
+                />
+            )
         }
+        return nodeArr;
     }
+
     return (
         data ? 
         <main className="bento-container">
-             <BentoSubContainer
-                classList={"zero "}
-                bentoNodes={handleNodes()}
+          <section className={`bento-sub-container zero top left`}>
+            <BentoSubContainer 
+                classList = {"bento-sub-container one top"}
+                bentoNodes={handleNodes(2)}
             />
-            <BentoSubContainer
-                classList={"zero "}
+            <BentoSubContainer 
+                classList = {"bento-sub-container one bottom"}
+                bentoNodes={handleNodes(2)}
             />
+          </section>
+          <BentoSubContainer 
+            classList = {"bento-sub-container zero top"}
+            bentoNodes={handleNodes(2)}
+          />
+          <BentoSubContainer 
+            classList = {"bento-sub-container zero bottom"}
+            bentoNodes={handleNodes(4)}
+          />
         </main> :
         <></>
     );
--- src/components/BentoSubContainer.js
+++ src/components/BentoSubContainer.js
@@ -2,10 +2,10 @@ import BentoNode from "./BentoNode";
 
 const BentoSubContainer = ({ classList, bentoNodes}) => {
     
-
+    console.log(bentoNodes);
     return (
         <section className={`bento-sub-container ${classList}`}>
-            
+            {bentoNodes.map((node)=> node )}
         </section>
     );
 }
